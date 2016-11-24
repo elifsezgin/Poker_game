929530194e0f4da8a849880547f329f9a6177b31
App Academy Student <student@appacademy.io>
2016-11-23 17:51:40 -0800

    Add scores for each hand case

--- lib/hand.rb
+++ lib/hand.rb
@@ -5,8 +5,21 @@ class Hand
   attr_reader :cards
 
   def initialize(cards)
-    @cards = cards.sort_by {|card| card.value}
+    @cards = cards.sort_by { |card| card.value }
   end
+
+  HAND_VALUES = {
+                :royal_flush => 10,
+                :straight_flush => 9,
+                :four_of_a_kind => 8,
+                :full_house => 7,
+                :flush  => 6,
+                :straight => 5,
+                :three_of_a_kind => 4,
+                :two_pairs => 3,
+                :one_pair => 2,
+                :high_card => 1
+              }
 
   def check_hand
     return :royal_flush if royal_flush?
